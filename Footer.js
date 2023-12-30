00ce9985581d56adc7e2daa71c4dbeacc8d87975
srikargupta1144 <127022212+srikargupta1144@users.noreply.github.com>
2023-12-30 11:44:06 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => (
+  <>
+    <div className="footer-block"></div>
+    <div className="footer">
+      <div className="footer-container">
+        
+        
+      </div>
+    </div>
+  </>
+);
+
+export default Footer;
